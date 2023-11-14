1372f0a2aa066ff26de7325da871bbe1c14cadbf
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-11-14 14:34:41 +0100

    Temp dep
    Took 35 minutes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -41,39 +42,43 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
-    /*
-    //ROOM
-    implementation("android.arch.persistence.room:runtime:1.1.1")
-    annotationProcessor("android.arch.persistence.room:compiler:1.1.1")
-    implementation("android.arch.lifecycle:extensions:1.1.1")
+    // Room
+    implementation("androidx.room:room-runtime:2.6.0")
+    ksp("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
 
-    //RECYCLERVIEW
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    //Recycler View
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
-    //TESTING
-    androidTestImplementation("android.arch.core:core-testing:1.1.1")
+    // Archcore Testing
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 
-    //ANIMATION
-    implementation("com.daimajia.androidanimations:library:2.4@aar")
+    // Picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
 
-    //DATABASE
+    // Daimajia (Dessin)
+    implementation("com.daimajia.easing:library:2.0@aar")
+    implementation("com.daimajia.androidanimations:library:2.3@aar")
+
+    // Amitshekhar (Debug)
     debugImplementation("com.amitshekhar.android:debug-db:1.0.6")
 
-    //GRAPHIQUE
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-    implementation("com.kofigyan.stateprogressbar:stateprogressbar:1.0.0")
-    implementation("com.makeramen:roundedimageview:2.3.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
-     */
+
+
 
     // Co Routine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
@@ -81,7 +86,7 @@ dependencies {
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.annotation:annotation:1.7.0")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
