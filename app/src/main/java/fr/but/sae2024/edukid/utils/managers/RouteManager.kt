926c2ecf40d1bae586a9ac34b1253e4b35cb35d0
Axel GAILLIARD <>
2023-11-15 22:32:38 +0100

    fun startGame

--- app/src/main/java/fr/but/sae2024/edukid/utils/managers/RouteManager.kt
+++ app/src/main/java/fr/but/sae2024/edukid/utils/managers/RouteManager.kt
@@ -18,18 +18,87 @@ class RouteManager {
 
         when (activitysName) {
 
-            ActivitysName.UserSelectionActivity -> context.startActivity(Intent().setClass(context, UserSelectionActivity::class.java))
+            ActivitysName.UserSelectionActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    UserSelectionActivity::class.java
+                )
+            )
 
-            ActivitysName.ThemeSelectionActivity -> context.startActivity(Intent().setClass(context, ThemeSelectionActivity::class.java))
+            ActivitysName.ThemeSelectionActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    ThemeSelectionActivity::class.java
+                )
+            )
 
-            else -> if(activitysName.toString().isNotEmpty()){
-                    Log.e(Log.ERROR.toString(),activitysName.toString() + "is not allowed for a name's activity")
-                }else {
-                    Log.e(Log.ERROR.toString(),"The name activity is empty")
-                }
+            ActivitysName.GameSelectionActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    GameSelectionActivity::class.java
+                )
+            )
+
+            ActivitysName.SubGameSelectionActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    SubGameSelectionActivity::class.java
+                )
+            )
+
+            ActivitysName.ResultGameActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    ResultGameActivity::class.java
+                )
+            )
+
+            ActivitysName.UserDetailActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    UserDetailActivity::class.java
+                )
+            )
+
+            ActivitysName.UserManagingActivity -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    UserManagingActivity::class.java
+                )
+            )
+
+            else -> if (activitysName.toString().isNotEmpty()) {
+                Log.e(
+                    Log.ERROR.toString(),
+                    activitysName.toString() + "is not allowed for a name's activity"
+                )
+            } else {
+                Log.e(Log.ERROR.toString(), "The name's activity is empty")
             }
         }
-
     }
 
+    public fun startGame(context : Context, gamesName: GamesName, wantToFinish: Boolean, animation: Boolean) {
+
+        when (gamesName) {
+
+
+            GamesName.Draw -> context.startActivity(
+                Intent().setClass(
+                    context,
+                    UserManagingActivity::class.java
+                )
+            )
+
+            else -> if (GamesName.toString().isNotEmpty()) {
+                Log.e(
+                    Log.ERROR.toString(),
+                    GamesName.toString() + "is not allowed for a name's game"
+                )
+            } else {
+                Log.e(Log.ERROR.toString(), "The name's game is empty")
+            }
+
+        }
+    }
 }
