67bdd06675b2ea1d3c96464f5b37bdc7c3d6925c
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-11-23 16:19:56 +0100

    (Don't work) start setup

--- app/src/main/java/fr/but/sae2024/edukid/views/users/UserSelectionActivity.kt
+++ app/src/main/java/fr/but/sae2024/edukid/views/users/UserSelectionActivity.kt
@@ -3,9 +3,18 @@ package fr.but.sae2024.edukid.views.users
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import fr.but.sae2024.edukid.R
 
 class UserSelectionActivity : AppCompatActivity() {
+
+    var UserRv : RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.user_selection_activity)
+
+        UserRv = findViewById(R.id.recyclerview_users)
+
+
     }
 }
---
+++ app/src/main/java/fr/but/sae2024/edukid/views/users/adapters/UserSelectionAdapter.kt
@@ -0,0 +1,11 @@
+package fr.but.sae2024.edukid.views.users.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserSelectionAdapter(private val User ) : RecyclerView.Adapter<UserSelectionAdapter.UserSelectionViewHolder>() {
+
+    inner class UserSelectionViewHolder(itemview : View) : RecyclerView.ViewHolder(itemview) {
+    }
+
+}
