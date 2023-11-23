ed1cc58d5110cc613613347b3363e0a2c6643e87
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2023-11-23 16:57:51 +0100

    Add Cards Models

--- app/src/main/java/fr/but/sae2024/edukid/models/games/Card.kt
+++ app/src/main/java/fr/but/sae2024/edukid/models/games/Card.kt
@@ -1,3 +1,20 @@
 package fr.but.sae2024.edukid.models.games
 
-data class Card()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cards")
+data class Card(
+
+    @PrimaryKey
+    @ColumnInfo(name = "value")
+    val value: String,
+
+    @ColumnInfo(name = "type")
+    val type: String? = null,
+
+    @ColumnInfo(name = "image")
+    val image: Int? = null,
+)
+
