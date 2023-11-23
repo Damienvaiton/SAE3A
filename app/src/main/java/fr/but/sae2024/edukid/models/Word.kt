f27e9ecde1b90d6f5ec37c742a06c9c57f99712e
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2023-11-23 16:29:03 +0100

    Word Entity

---
+++ app/src/main/java/fr/but/sae2024/edukid/models/Word.kt
@@ -0,0 +1,16 @@
+package fr.but.sae2024.edukid.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "words")
+data class Word (
+
+    @PrimaryKey
+    @ColumnInfo(name = "word")
+    val word: String? = null,
+
+    @ColumnInfo(name = "image")
+    val image: Int? = null
+){}
