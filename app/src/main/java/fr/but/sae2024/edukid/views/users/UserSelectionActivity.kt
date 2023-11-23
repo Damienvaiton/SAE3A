2dfd1d2ee5d23ee0be98eeb5dfa615252a0ba2fa
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-11-23 14:54:04 +0100

    Create UserSlec

---
+++ app/src/main/java/fr/but/sae2024/edukid/views/users/UserSelectionActivity.kt
@@ -0,0 +1,11 @@
+package fr.but.sae2024.edukid.views.users
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
