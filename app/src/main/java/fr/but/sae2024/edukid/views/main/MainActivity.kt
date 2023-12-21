f30bd8f2e2690feda9469cbf67980dfb6ecef115
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-12-21 15:43:02 +0100

    Sonar lint
    Took 2 minutes

--- app/src/main/java/fr/but/sae2024/edukid/views/main/MainActivity.kt
+++ app/src/main/java/fr/but/sae2024/edukid/views/main/MainActivity.kt
@@ -2,9 +2,6 @@ package fr.but.sae2024.edukid.views.main
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.Debug
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import fr.but.sae2024.edukid.R
 
@@ -12,7 +9,6 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashScreen = installSplashScreen()
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main);
