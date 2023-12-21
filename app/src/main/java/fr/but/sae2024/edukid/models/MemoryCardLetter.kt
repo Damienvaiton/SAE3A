cd95443ffe260a6b0f9f25f8d9e07cdd201c1bca
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2023-12-21 16:52:37 +0100

    Add 4 missing models

---
+++ app/src/main/java/fr/but/sae2024/edukid/models/MemoryCardLetter.kt
@@ -0,0 +1,26 @@
+package fr.but.sae2024.edukid.models
+
+class MemoryCardLetter(value: String?, font: Int) : MemoryCard(value) {
+    private var font = 0
+
+    init {
+        this.font = font
+    }
+
+    constructor(memoryCardLetter: MemoryCardLetter) : this(memoryCardLetter.getValue(), memoryCardLetter.getFont())
+
+    override fun getDrawableImage(): Int {
+        return 0 // À remplacer par l'ID de l'image appropriée
+    }
+
+    override fun getFont(): Int {
+        return font
+    }
+
+    override fun setDrawableImage(drawableImage: Int) {
+    }
+
+    override fun setFont(font: Int) {
+        this.font = font
+    }
+}
