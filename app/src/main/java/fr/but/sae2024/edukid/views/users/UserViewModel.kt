b40b7ce227ea08c5b560884c48562b7f8fae47e2
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-12-31 16:35:41 +0100

    add work
    Took 16 minutes

--- app/src/main/java/fr/but/sae2024/edukid/views/users/UserViewModel.kt
+++ app/src/main/java/fr/but/sae2024/edukid/views/users/UserViewModel.kt
@@ -8,9 +8,7 @@ import fr.but.sae2024.edukid.database.EdukidDatabase
 import fr.but.sae2024.edukid.database.dao.UserDao
 import fr.but.sae2024.edukid.models.entities.app.User
 import fr.but.sae2024.edukid.repositories.UserRepository
-import fr.but.sae2024.edukid.utils.enums.UserRole
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class UserViewModel : ViewModel() {
 
@@ -24,48 +22,11 @@ class UserViewModel : ViewModel() {
 
     private val userRepo = UserRepository
 
-
-    fun displayListUser(listUser: List<User>) {
-
-        if (listUser.isEmpty()) {
-            Timber.tag("UserSelectionActivity").e("List is empty")
-        }
-        else {
-            for (user in listUser) {
-                Timber.tag("UserSelectionActivity").e("User : $user \n")
-            }
-        }
-
+    fun createUserChild(username : String, context : Context){
+        val user = User(username)
+        insertUser(user)
     }
-    fun addManyUser(){
-            viewModelScope.launch {
 
-                userDao.insertUser(
-                    User(
-                        "admin",
-                        "admin",
-                        "admin@mailtest.com",
-                        null,
-                        1,
-                        UserRole.PARENT,
-                        0
-                    )
-                )
-                for (i in 1..3) {
-                    userDao.insertUser(
-                        User(
-                            "child$i",
-                            "child$i",
-                            "child$i@mailtest.com",
-                            null,
-                            1,
-                            UserRole.CHILD,
-                            0
-                        )
-                    )
-                }
-            }
-        }
 
     fun getListUser(context : Context){
         viewModelScope.launch {
--- app/src/main/java/fr/but/sae2024/edukid/views/users/resume/UserAddActivity.kt
+++ app/src/main/java/fr/but/sae2024/edukid/views/users/resume/UserAddActivity.kt
@@ -1,17 +1,32 @@
 package fr.but.sae2024.edukid.views.users.resume
 
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import fr.but.sae2024.edukid.R
+import fr.but.sae2024.edukid.utils.enums.ActivityName
+import fr.but.sae2024.edukid.utils.managers.RouteManager
+import fr.but.sae2024.edukid.views.users.UserViewModel
 import timber.log.Timber
 
 
 class UserAddActivity : AppCompatActivity()  {
 
+    val userViewModel by viewModels<UserViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_add_activity)
         Timber.tag("UserAddActivity").e("onCreate called")
+
+
+        val username = findViewById<TextInputEditText>(R.id.textField_userName)
+        val validateButton = findViewById<Button>(R.id.buttonValider_userEditPage)
+        validateButton.setOnClickListener {
+            userViewModel.createUserChild(username.text.toString(), this)
+            RouteManager.startActivity(this, ActivityName.UserSelectionActivity, false, true)
+        }
     }
 
 }
