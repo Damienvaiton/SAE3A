7fdadfa937fedd3862eebace5678281ee39424e4
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2024-01-16 09:40:38 +0100

    Game Adapter

--- app/src/main/java/fr/but/sae2024/edukid/views/games/menu/GameSelectionActivity.kt
+++ app/src/main/java/fr/but/sae2024/edukid/views/games/menu/GameSelectionActivity.kt
@@ -1,4 +1,18 @@
 package fr.but.sae2024.edukid.views.games.menu
 
-class GameSelectionActivity {
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import fr.but.sae2024.edukid.R
+
+class GameSelectionActivity : AppCompatActivity() {
+
+    private val gameViewModel by viewModels<GameViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_menu)
+
+
+    }
 }
